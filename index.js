f2b161cbc5018c88920307293d43ed992a0fdb0c
Prakriti Pokharel <prakriti.pokharel@globalyhub.com>
2023-04-12 10:36:48 +0545

    add type submit on button

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const opt1 = document.getElementById("choice1");
+const labelElement1= opt1.previousElementSibling;
 const opt2 = document.getElementById("choice2");
+const labelElement2= opt2.previousElementSibling;
 const decision = document.createElement("h2");
 const container = document.getElementById("container");
 const form = document.getElementById("myForm");
 const number = Math.floor(Math.random() * 10);
-
+console.log(opt1.value);
 const submitBtn = document.getElementById("submitBtn");
-submitBtn.addEventListener("click",()=>{
-form .submit();
-console.log("submitted")
-})
-function submitForm() {
-    // opt1.style.display = "none";
-    // opt2.style.display = "none";
-    // if (number % 2 == 0) {
-    //     decision.innerHTML = `You should go with option 1 i.e ${opt2.value}`
-    // }
-    // else {
-    //     decision.innerHTML = `You should go with option 1 i.e ${opt21.value}`
-    // }
-    // container.appendChild(decision);
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+    opt1.style.display = "none";
+    opt2.style.display = "none";
+    labelElement1.style.display= "none";
+    labelElement2.style.display= "none";
+    submitBtn.style.display = "none"
+    if (number % 2 == 0) {
+        decision.innerHTML = `You should go with option 1 i.e ${opt2.value}`
+    }
+    else {
+        decision.innerHTML = `You should go with option 1 i.e ${opt1.value}`
+    }
+    container.appendChild(decision);
     console.log("gesdafZ")
 }
